f93a4a3ad4301f8532bab411e03f37287a2602ed
Jason Kölker <jason@koelker.net>
2011-03-08 21:05:10 -0600

    add twisted plugins

--- setup.py
+++ setup.py
@@ -14,7 +14,9 @@ setup(name='shaft',
       author_email='jason@koelker.net',
       url='',
       license='',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']) + \
+               ['twisted.plugins'],
+      package_data = {'twisted': ['plugins/shaft_plugin.py'],},
       include_package_data=True,
       zip_safe=False,
       install_requires=[
