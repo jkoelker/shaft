4514c3037829c9c64f955c78f9c19f4d66b14358
Jason Kölker <jason@koelker.net>
2011-04-09 20:37:58 -0500

    add minecraft resource

---
+++ shaft/resources/minecraft.py
@@ -0,0 +1,37 @@
+from twisted.internet import defer
+from twisted.web import resource, server
+
+from shaft import backup, minecraft, settings, supervisor
+
+import os
+
+class Minecraft(resource.Resource):
+    def render_POST(self, requst):
+
+        def started(result):
+            request.finish()
+
+        def stopped(result):
+            request.finish()
+
+        data = json.loads(request.content.getvalue())
+        action = data.get("action", None)
+
+        s = supervisor.get()
+
+        if action is None:
+             return resource.ErrorPage(http.BAD_REQUEST,
+                                       http.RESPONSES[http.BAD_REQUEST],
+                                       "action required")
+
+        else if action.lower() == "start":
+            d = s.startMinecraft(jar=settings.config["minecraft"]["jar"],
+                                 path=settings.config["minecraft"]["home"])
+            d.addCallback(started)
+
+        else if action.lower() == "stop":
+            d = s.stopMinecraft()
+            d.addCallback(stopped)
+
+        return server.NOT_DONE_YET
+
