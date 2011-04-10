88803310c5744879621f224b5fb1cdaed2e7a415
Jason Kölker <jason@koelker.net>
2011-04-09 20:40:31 -0500

    wire up the minecraft resource

--- shaft/web.py
+++ shaft/web.py
@@ -1,6 +1,9 @@
 from twisted.web import resource
 
+from shaft.resources import minecraft
+
 def getRoot():
     root = resource.Resource()
+    root.putChild("minecraft", minecraft.Minecraft())
 
     return root
